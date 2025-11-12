b161a7694cc8cdca7acb0c8e4ba7fca353bf2675
Arkadiy js <Vologodskiy20@gmail.com>
2025-11-12 22:52:29 +0500

    The sketches

--- radio-fun-app/src/App.jsx
+++ radio-fun-app/src/App.jsx
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="App">hello world</div>
+      <div className="App">
+        <div className="Header">
+          radio fun very cool the best world first bang bang
+        </div>
+        <div className="Content">
+          link radio-fun book and quiz question-test
+        </div>
+      </div>
     </>
   );
 }
